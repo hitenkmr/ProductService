915e7a0f28a6b1dbc8109b2a04b24f2eaecf5960
Hitender kumar <hitender.hk@gmail.com>
2024-07-10 16:48:52 +0530

    set HttpComponentsClientHttpRequestFactory on restTemplate to make PATCH request work

--- src/main/java/com/scaler/productservice/configurations/ApplicationConfig.java
+++ src/main/java/com/scaler/productservice/configurations/ApplicationConfig.java
@@ -2,12 +2,15 @@ package com.scaler.productservice.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ApplicationConfig {
     @Bean
     public RestTemplate getRestTemplate() {
-        return new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+        return restTemplate;
     }
 }
